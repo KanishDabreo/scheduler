7bcf90f59359544ffd25ba03ecc2391c102aa4a9
Kanisha Dabreo <kanishaddabreo@gmail.com>
2021-11-09 15:23:02 -0500

    bug fix-- error failed to compile

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,31 +1,30 @@
 import React from "react";
 import classNames from "classnames";
-import DayListItem from "./DayListItem.scss";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-  
+  const { name, spots, setDay, selected } = props;
+
   const dayClass = classNames("day-list__item", {
-    "day-list__item--selected": props.selected,
-    "day-list__item--full": props.spots === 0
+    "day-list__item--selected": selected,
+    "day-list__item--full": spots === 0,
   });
-  
-  const formatSpots = (spots) => {
+
+  function formatSpots() {
+    let spotsRemaining;
+    spotsRemaining = `${spots} spots remaining`
     if (spots === 0) {
-      return "no spots left"
+      spotsRemaining = "no spots remaining";
+    } if (spots === 1) {
+      spotsRemaining = "1 spot remaining";
     }
-    
-    if (spots === 1) {
-      return "1 spot left"
-    }
-    return `${spots} spots left`
+    return spotsRemaining;
   }
 
-  const spots = formatSpots(props.spots)
-
   return (
-    <li className={dayClass} onClick={() => setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    <li className={dayClass} onClick={() => setDay(name)}>
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">{formatSpots()} remaining</h3>
     </li>
   );
 }
