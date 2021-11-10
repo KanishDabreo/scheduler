52f42f1e935e35364159bc55c7e1385b6400e942
Kanisha Dabreo <kanishaddabreo@gmail.com>
2021-11-09 19:52:57 -0500

    bug fix in show.js incorrect spelling of interviewer

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,24 +5,25 @@ import "components/InterviewerList.scss";
 
 //Try sending a string to the interviewers prop that is now required to be an array (e.g. try the .toString() function). Check the console to find the warning. When you see the error, revert the code and ensure that the prop is an array again.w8d2-2
 function InterviewerList(props) {
-  const interviewers = props.interviewers.map((interviewer) => {
-    return (
-      <InterviewerListItem
-        key={interviewer.id}
-        name={interviewer.name}
-        spots={interviewer.spots}
-        selected={interviewer.id === props.value}
-        setInterviewer={() => props.onChange(interviewer)} />
-    )
-  });
+  const { interviewers, onChange, value } = props;
+  const interviewersList = interviewers.map(interviewer =>
+    <InterviewerListItem
+      key={interviewer.id}
+      name={interviewer.name}
+      spots={interviewer.spots}
+      selected={interviewer.id === value}
+      setInterviewer={() => onChange(interviewer.id)} />
+  )
 
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list">{interviewers}</ul>
+      <ul className="interviewers__list">
+        {interviewersList}
+      </ul>
     </section>
   )
-}
+};
 
 InterviewerList.propTypes = {
   interviewers: PropTypes.array.isRequired
