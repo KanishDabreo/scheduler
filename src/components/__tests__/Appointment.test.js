d37704d4a87e5fe4bab17ff31bf96c5060882539
Kanisha Dabreo <kanishaddabreo@gmail.com>
2021-11-09 19:25:06 -0500

    fix import issue

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,27 +1,21 @@
-/*
-  We are rendering `<Application />` down below, so we need React.createElement
-*/
 import React from "react";
 
 /*
   We import our helper functions from the react-testing-library
   The render function allows us to render Components
 */
-import { render } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 
 /*
   We import the component that we are testing
 */
-import Application from "components/Application";
+import Appointment from "components/Appointment"
 
 /*
   A test that renders a React Component
 */
-it("renders without crashing", () => {
-  render(<Application />);
-});
+afterEach(cleanup);
 
-//example 
 describe("Appointment", () => {
   it("renders without crashing", () => {
     render(<Appointment />);
